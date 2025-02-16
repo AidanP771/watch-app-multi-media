fe630da0f602faec4904c5272ffb8f9dbe89e97e
AidanP771 <aidanm771@outlook.com>
2025-02-16 18:51:10 -0500

    qs

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import CounterButton from "./components/CounterButton";
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <h1>This is a test</h1>
       <CounterButton />
     </div>
