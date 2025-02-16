e72fe5fe651389995a21ac6ecce28a986447182d
AidanP771 <aidanm771@outlook.com>
2025-02-16 16:01:35 -0500

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,3 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-=======
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
@@ -42,32 +6,11 @@ import TestButton from "./components/TestButton";
 
 function App() {
   return (
-=======
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import TestButton from "./components/TestButton";
-
-function App() {
-  return (
->>>>>>> Stashed changes
-=======
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import TestButton from "./components/TestButton";
-
-function App() {
-  return (
->>>>>>> Stashed changes
     <div>
       <h1>This is a test</h1>
       <TestButton />
     </div>
   );
->>>>>>> Stashed changes
 }
 
-export default App
+export default App;
