0ce6bb8f022afa2e03ee5008aeb42b021404cf29
AidanP771 <aidanm771@outlook.com>
2025-02-16 15:58:26 -0500

    created component folder npm install react-extras

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -40,6 +41,16 @@ import TestButton from "./components/TestButton";
 
 function App() {
   return (
+=======
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import TestButton from "./components/TestButton";
+
+function App() {
+  return (
+>>>>>>> Stashed changes
     <div>
       <h1>This is a test</h1>
       <TestButton />
