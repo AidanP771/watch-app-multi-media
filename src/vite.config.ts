17527f63959b6f86c6d8eff0af78188f70f15f1f
AidanP771 <aidanm771@outlook.com>
2025-02-20 01:38:08 -0500

    config fixes for deployment

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: "/watch-app-multi-media/", // Ensure this matches your GitHub repo name
 });
