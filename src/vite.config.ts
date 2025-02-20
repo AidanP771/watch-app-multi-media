48e67e8e3e7274788519fd662125e5465e992c2c
AidanP771 <aidanm771@outlook.com>
2025-02-20 02:06:18 -0500

    Update vite.config.ts

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/watch-app-multi-media/", // Ensure this matches your GitHub repo name
+  base: "/watch-app-multi-media/",
 });
